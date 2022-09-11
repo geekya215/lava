7ca70367e85d6f8c3908131a2e880b4c727b283c
Lin Yang <geekya215@gmail.com>
2022-09-12 00:42:37 +0800

    fix app expr convert error

--- src/main/java/io/geekya215/lava/expr/Expr.java
+++ src/main/java/io/geekya215/lava/expr/Expr.java
@@ -3,10 +3,13 @@ package io.geekya215.lava.expr;
 import io.geekya215.lava.exceptions.ParserException;
 import io.geekya215.lava.nodes.*;
 
+import java.util.ArrayList;
+
 public sealed interface Expr permits
-    RefExpr, BoolExpr, IntegerExpr, ConsExpr, NilExpr,
+    RefExpr, LambdaExpr, AppExpr,
+    BoolExpr, IntegerExpr, NilExpr,
     PlusExpr, MinusExpr, MulExpr, DivExpr,
-    CarExpr, CdrExpr, QuoteExpr {
+    ConsExpr, CarExpr, CdrExpr, QuoteExpr {
     static Expr from(SExprNode sexpr) throws ParserException {
         return switch (sexpr) {
             case SymbolNode symbolNode -> new RefExpr(symbolNode.val());
@@ -28,10 +31,10 @@ public sealed interface Expr permits
                                 case "car" -> new CarExpr(from(arg));
                                 case "cdr" -> new CdrExpr(from(arg));
                                 case "quote" -> new QuoteExpr(from(arg));
-                                default -> throw new ParserException("unexpected S-expression: " + sexpr);
+                                default -> buildApplication(consNode);
                             };
                         }
-                        if (size == 3) {
+                        else if (size == 3) {
                             var car = nodes.get(1);
                             var cdr = nodes.get(2);
                             yield switch (symbolNode.val()) {
@@ -40,10 +43,21 @@ public sealed interface Expr permits
                                 case "-" -> new MinusExpr(from(car), from(cdr));
                                 case "*" -> new MulExpr(from(car), from(cdr));
                                 case "/" -> new DivExpr(from(car), from(cdr));
-                                default -> throw new ParserException("unexpected S-expression: " + sexpr);
+                                case "lambda" -> {
+                                    var params = new ArrayList<String>();
+                                    for (var p : car.toList()) {
+                                        if (p instanceof SymbolNode sym) {
+                                            params.add(sym.val());
+                                        } else {
+                                            throw new ParserException("invalid parameter: " + p);
+                                        }
+                                    }
+                                    yield new LambdaExpr(params, from(cdr));
+                                }
+                                default -> buildApplication(consNode);
                             };
                         } else {
-                            throw new ParserException("unexpected S-expression: " + sexpr);
+                            yield buildApplication(consNode);
                         }
                     } else {
                         throw new ParserException("unexpected S-expression: " + sexpr);
@@ -51,5 +65,14 @@ public sealed interface Expr permits
                 }
             }
         };
+    }
+
+    static AppExpr buildApplication(ConsNode consNode) throws ParserException {
+        var fun = from(consNode.car());
+        var args = new ArrayList<Expr>();
+        for (var arg : consNode.cdr().toList()) {
+            args.add(from(arg));
+        }
+        return new AppExpr(fun, args);
     }
 }
