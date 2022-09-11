42dcc51abd029473ebd56a9e85652f21512c8f40
Lin Yang <geekya215@gmail.com>
2022-09-11 22:35:54 +0800

    fix NilNode toList method error

--- src/main/java/io/geekya215/lava/nodes/NilNode.java
+++ src/main/java/io/geekya215/lava/nodes/NilNode.java
@@ -1,11 +1,12 @@
 package io.geekya215.lava.nodes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record NilNode() implements SExprNode {
     @Override
     public List<SExprNode> toList() {
-        return List.of();
+        return new ArrayList<>();
     }
 
     @Override
