e6794286768e8bfb63cfc8fae620175392ed317b
Lin Yang <geekya215@gmail.com>
2022-09-29 23:49:00 +0800

    refactor Lava main function

--- src/main/java/io/geekya215/lava/Lava.java
+++ src/main/java/io/geekya215/lava/Lava.java
@@ -11,6 +11,7 @@ import org.jline.reader.impl.completer.StringsCompleter;
 import org.jline.terminal.TerminalBuilder;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.Scanner;
@@ -19,7 +20,10 @@ public class Lava {
     public static void main(String[] args) throws IOException {
         var standardEnv = Interpreter.initialStandardEnv();
         var initialEnv = Env.extend(standardEnv);
+
         String filePath = null;
+        Expr res = null;
+
         var terminal = TerminalBuilder.builder()
             .jansi(true)
             .jna(false)
@@ -29,63 +33,65 @@ public class Lava {
             .completer(new ArgumentCompleter(new StringsCompleter("load"), new FileNameCompleter(), NullCompleter.INSTANCE))
             .build();
 
+        // loop
         while (true) {
             try {
                 var input = lineReader.readLine(Constants.PROMPT);
                 if (input.isBlank()) {
+                    continue;
                 } else if (Objects.equals(input, "exit")) {
-                    break;
+                    System.out.println("\nBye.");
+                    System.exit(0);
                 } else if (Objects.equals(input, "reload")) {
                     if (filePath == null) {
-                        System.out.println("please load file first");
+                        System.out.println("error: please load file first");
                         continue;
                     }
-                    try (var sc = new Scanner(new File(filePath))) {
-                        Expr result = null;
-                        while (sc.hasNext()) {
-                            var line = sc.nextLine();
-                            if (line.isBlank()) {
-                                continue;
-                            }
-                            var tokens = Tokenizer.tokenize(line);
-                            var expr = Parser.parse(new Ref<>(tokens));
-                            result = Interpreter.eval(expr, initialEnv);
-                        }
-                        System.out.println(result);
-                    }
+                    res = loadFile(filePath, initialEnv);
                 } else if (input.startsWith("load")) {
                     filePath = input.substring(4).stripLeading().stripTrailing();
-                    try (var sc = new Scanner(new File(filePath))) {
-                        Expr result = null;
-                        while (sc.hasNext()) {
-                            var line = sc.nextLine();
-                            if (line.isBlank()) {
-                                continue;
-                            }
-                            var tokens = Tokenizer.tokenize(line);
-                            var expr = Parser.parse(new Ref<>(tokens));
-                            result = Interpreter.eval(expr, initialEnv);
-                        }
-                        System.out.println(result);
-                    } catch (Exception e) {
-                        System.out.println("file not exist");
-                    }
+                    res = loadFile(filePath, initialEnv);
                 } else {
-                    var tokens = Tokenizer.tokenize(input);
-                    var expr = Parser.parse(new Ref<>(tokens));
-                    var result = Interpreter.eval(expr, initialEnv);
-                    System.out.print(Constants.INDICATOR);
-                    System.out.println(result);
+                    res = eval(input, initialEnv);
                 }
+
+                print(res);
+
             } catch (UserInterruptException e) {
-                System.out.println("\nBye.");
-                break;
+                System.out.println("\nctrl-c interrupt");
+                System.exit(1);
             } catch (ParserException | EvalException e) {
                 System.out.println("error: " + e.getMessage());
+            } catch (FileNotFoundException e) {
+                System.out.println("error: file not exist");
             } catch (StackOverflowError error) {
                 System.err.println("\ncrashed cause: call stack overflow");
                 System.exit(1);
             }
         }
+    }
+
+    public static Expr eval(String input, Env env) {
+        var tokens = Tokenizer.tokenize(input);
+        var expr = Parser.parse(new Ref<>(tokens));
+        return Interpreter.eval(expr, env);
+    }
+
+    public static Expr loadFile(String path, Env env) throws FileNotFoundException {
+        try (var sc = new Scanner(new File(path))) {
+            Expr res = null;
+            while (sc.hasNext()) {
+                var line = sc.nextLine();
+                if (line.isBlank()) {
+                    continue;
+                }
+                res = eval(line, env);
+            }
+            return res;
+        }
+    }
+
+    public static void print(Expr expr) {
+        System.out.printf("%s%s\n", Constants.INDICATOR, expr);
     }
 }
