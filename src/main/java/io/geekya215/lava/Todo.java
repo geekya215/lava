e028dfa2830fea68f7b63e3282763f2ced7d1f77
Lin Yang <geekya215@gmail.com>
2023-07-12 13:54:32 +0800

    add Todo class

---
+++ src/main/java/io/geekya215/lava/Todo.java
@@ -0,0 +1,10 @@
+package io.geekya215.lava;
+
+public class Todo {
+    // refactor with JDK 21
+    // enable call/cc
+    // structure based pattern match
+    // variable auto complete by jline
+    // build cross-platform binary by graalvm
+    // bootstrap
+}
