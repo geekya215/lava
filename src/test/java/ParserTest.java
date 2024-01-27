a389029d0dee0e232613d0bf117922092ab9b68d
Lin Yang <geekya215@gmail.com>
2024-01-27 15:25:21 +0800

    refactor parser

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,5 +1,8 @@
-import io.geekya215.lava.*;
-
+import io.geekya215.lava.parser.Expr;
+import io.geekya215.lava.parser.Parser;
+import io.geekya215.lava.tokenizer.Keywords;
+import io.geekya215.lava.tokenizer.Operators;
+import io.geekya215.lava.tokenizer.Token;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -7,38 +10,71 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
-    List<Token> tokenize(String input) {
-        return Lexer.tokenize(Lexer.preprocess(input));
-    }
     @Test
-    void getNumber() {
-        var tokens = tokenize("1");
-        var actualExpr = Parser.parse(new Ref<>(tokens));
-        var expectedExpr = new Expr.Number(1);
-        assertEquals(expectedExpr, actualExpr);
+    void parseNumber() {
+        Expr actual = Parser.parse("123");
+        Expr excepted = new Expr.Atom(new Token.Number("123"));
+        assertEquals(excepted, actual);
     }
 
     @Test
-    void getSymbol() {
-        var tokens = tokenize("abc");
-        var actualExpr = Parser.parse(new Ref<>(tokens));
-        var expectedExpr = new Expr.Symbol("abc");
-        assertEquals(expectedExpr, actualExpr);
+    void parseParenthesis() {
+        Expr actual = Parser.parse("()");
+        Expr excepted = new Expr.Vec(List.of());
+        assertEquals(excepted, actual);
     }
 
     @Test
-    void getQuote() {
-        var tokens = tokenize("'abc");
-        var actualExpr = Parser.parse(new Ref<>(tokens));
-        var expectedExpr = new Expr.Quote(new Expr.Symbol("abc"));
-        assertEquals(expectedExpr, actualExpr);
+    void parseQuote() {
+        Expr actual = Parser.parse("(list 'a 'b 'c)");
+        Expr excepted = new Expr.Vec(List.of(
+                new Expr.Atom(new Token.Keyword(new Keywords.LIST())),
+                new Expr.Quote(new Expr.Atom(new Token.Symbol("a"))),
+                new Expr.Quote(new Expr.Atom(new Token.Symbol("b"))),
+                new Expr.Quote(new Expr.Atom(new Token.Symbol("c")))
+        ));
+        assertEquals(excepted, actual);
     }
 
     @Test
-    void getList() {
-        var tokens = tokenize("(a b 1)");
-        var actualExpr = Parser.parse(new Ref<>(tokens));
-        var expectedExpr = new Expr.List(List.of(new Expr.Symbol("a"), new Expr.Symbol("b"), new Expr.Number(1)));
-        assertEquals(expectedExpr, actualExpr);
+    void parseFib() {
+        Expr actual = Parser.parse("(def fib (fn (n) (if (<= n 1) (n) (+ (fib (- n 1)) (fib (- n 2))))))");
+        Expr excepted = new Expr.Vec(List.of(
+                new Expr.Atom(new Token.Keyword(new Keywords.DEF())),
+                new Expr.Atom(new Token.Symbol("fib")),
+                new Expr.Vec(List.of(
+                        new Expr.Atom(new Token.Keyword(new Keywords.FN())),
+                        new Expr.Vec(List.of(
+                                new Expr.Atom(new Token.Symbol("n"))
+                        )),
+                        new Expr.Vec(List.of(
+                                new Expr.Atom(new Token.Keyword(new Keywords.IF())),
+                                new Expr.Vec(List.of(
+                                        new Expr.Atom(new Token.Operator(new Operators.LtEq())),
+                                        new Expr.Atom(new Token.Symbol("n")),
+                                        new Expr.Atom(new Token.Number("1")))),
+                                new Expr.Vec(List.of(
+                                        new Expr.Atom(new Token.Symbol("n")))),
+                                new Expr.Vec(List.of(
+                                        new Expr.Atom(new Token.Operator(new Operators.Plus())),
+                                        new Expr.Vec(List.of(
+                                                new Expr.Atom(new Token.Symbol("fib")),
+                                                new Expr.Vec(List.of(
+                                                        new Expr.Atom(new Token.Operator(new Operators.Minus())),
+                                                        new Expr.Atom(new Token.Symbol("n")),
+                                                        new Expr.Atom(new Token.Number("1"))))
+                                        )),
+                                        new Expr.Vec(List.of(
+                                                new Expr.Atom(new Token.Symbol("fib")),
+                                                new Expr.Vec(List.of(
+                                                        new Expr.Atom(new Token.Operator(new Operators.Minus())),
+                                                        new Expr.Atom(new Token.Symbol("n")),
+                                                        new Expr.Atom(new Token.Number("2"))))
+                                        ))
+                                ))
+                        ))
+                ))
+        ));
+        assertEquals(excepted, actual);
     }
 }
